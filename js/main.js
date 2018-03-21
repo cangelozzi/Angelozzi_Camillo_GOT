3845aa7c9eed66613246707b6bbea4a414efc551
camgelozzi <cangelozzi@gmail.com>
2018-03-21 15:37:49 -0400

    update

--- js/main.js
+++ js/main.js
@@ -1,6 +1,40 @@
 // Javascript Document
-(()=> {
-  console.log('JS initialized!')
+(() => {
+  console.log('JS initialized!');
 
-  
+  // VARIABLES STACK
+  const vidPlayer = document.querySelector('video');
+  const playButton = document.querySelectorAll('button')[0];
+  const pauseButton = document.querySelectorAll('button')[1];
+  const rewindButton = document.querySelectorAll('button')[2];
+
+
+  // FUNCTION STACK
+  function volOn() {
+    vidPlayer.muted = false;
+  }
+
+  function volOff() {
+    vidPlayer.muted = true;
+  }
+
+  function rewindVid() {
+    vidPlayer.currentTime -= 5;
+  }
+
+  function playVid() {
+    vidPlayer.play();
+  }
+
+  function pauseVid() {
+    vidPlayer.pause();
+  }
+
+  // ADD EVENT LISTENER STACK
+  vidPlayer.addEventListener('mouseover', volOn);
+  vidPlayer.addEventListener('mouseout', volOff);
+  rewindButton.addEventListener('click', rewindVid);
+  playButton.addEventListener('click', playVid);
+  pauseButton.addEventListener('click', pauseVid);
+
 })();
