9bad6d5d5501296800b1f1b2f99eadc5f9bb6296
camgelozzi <cangelozzi@gmail.com>
2018-03-21 18:23:28 -0400

    video functions update

--- js/main.js
+++ js/main.js
@@ -1,16 +1,42 @@
 // Javascript Document
+
 (() => {
   console.log('JS initialized!');
 
+  String.prototype.capIt = function () {
+    return this.replace(this.charAt(), this.charAt().toUpperCase());
+  }
+
   // VARIABLES STACK
   const sigils = document.querySelectorAll('.sigilContainer');
-  sigils.forEach(sigil => addEventListener('click', loadMovie));
+  const lightbox = document.querySelector('.lightbox');
+  const closeLightbox = lightbox.querySelector('.close-lightbox');
+  const vidPlayer = document.querySelector('video');
+
+
 
   // FUNCTION STACK
   function loadMovie() {
-    debugger;
+
+    // turn on the lightbox
+    lightbox.classList.add('show-lightbox');
+
+    // 2. grab the right video based on clas name
+    var House = this.className.split(' ')[1].capIt();
+    console.log(House)
+
+    vidPlayer.play();
   }
 
-  // ADD EVENT LISTENER STACK
+  function closeBox() {
+    lightbox.classList.remove('show-lightbox');
+    vidPlayer.pause();
+    vidPlayer.currentTime = 0;
+  }
 
+
+
+  // ADD EVENT LISTENER STACK
+  sigils.forEach(sigil => sigil.addEventListener('click', loadMovie));
+  closeLightbox.addEventListener('click', closeBox);
 })();
