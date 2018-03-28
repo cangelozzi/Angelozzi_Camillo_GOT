790101fd1cd86ded5d4d262310d1628cead12459
camgelozzi <cangelozzi@gmail.com>
2018-03-28 15:49:31 -0400

    video controls

--- js/main.js
+++ js/main.js
@@ -12,7 +12,11 @@
   const lightbox = document.querySelector('.lightbox');
   const closeLightbox = lightbox.querySelector('.close-lightbox');
   const vidPlayer = document.querySelector('video');
-
+  const changeMe = document.querySelector('#changeMe');
+  const videoTitle = document.querySelector('#videoTitle');
+  const playPause = document.querySelector('.play-pause');
+  const rWnd = document.querySelector('.rewind');
+  const ffWd = document.querySelector('.forward');
 
 
   // FUNCTION STACK
@@ -22,9 +26,16 @@
     lightbox.classList.add('show-lightbox');
 
     // 2. grab the right video based on clas name
-    var House = this.className.split(' ')[1].capIt();
-    console.log(House)
+    var house = this.className.split(' ')[1].capIt();
+    console.log(house)
 
+    // 3. put path together and make the video play
+    vidPlayer.src = `video/House-${house}.${vidPlayer.currentSrc.split('.')[1]}`;
+
+    changeMe.textContent = house;
+    videoTitle.textContent = house;
+
+    vidPlayer.load();
     vidPlayer.play();
   }
 
@@ -34,9 +45,32 @@
     vidPlayer.currentTime = 0;
   }
 
+  function togglePlay() {
+    var theSVG = this.firstElementChild;
+
+    if (vidPlayer.paused) {
+      theSVG.dataset.icon = "pause-circle";
+      vidPlayer.play();
+    } else {
+      theSVG.dataset.icon = "play-circle";
+      vidPlayer.pause();
+    }
+  }
+
+  function rWindVid() {
+    console.log('rewind')
+  }
+
+  function ffWdVid() {
+    console.log('forward')
+  }
 
 
   // ADD EVENT LISTENER STACK
   sigils.forEach(sigil => sigil.addEventListener('click', loadMovie));
   closeLightbox.addEventListener('click', closeBox);
+  vidPlayer.addEventListener('ended', closeBox);
+  playPause.addEventListener('click', togglePlay);
+  rWnd.addEventListener('click', rWindVid);
+  ffWd.addEventListener('click', ffWdVid);
 })();
